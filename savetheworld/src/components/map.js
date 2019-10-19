ee5ec80128aa61841794cb005fc82bb7fd9ecf38
Rokcas <rokas0090@gmail.com>
2019-10-19 17:49:49 +0300

    Add map

---
+++ savetheworld/src/components/map.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+    width: '100%',
+    height: '100%',
+  };
+
+class MapChart extends React.Component{
+    render() {
+        return (
+            <Map
+              google={this.props.google}
+              zoom={8}
+              style={mapStyles}
+              initialCenter={{ lat: 47.444, lng: -122.176}}
+            />
+        );
+      }
+}
+
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyAyNJ3YFC0jTvxEBj5uSU1mc7kjNIcZqck'
+  })(MapChart);
