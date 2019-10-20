9f73abb31c403f1712f4bb733aea148cbd71e421
Rokcas <rokas0090@gmail.com>
2019-10-20 09:48:35 +0300

    Remove unused code, add some trash code for drawing polygons

--- react-redux-registration-login-example-master/src/_components/map.js
+++ react-redux-registration-login-example-master/src/_components/map.js
@@ -1,34 +1,72 @@
-import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import React from "react";
+import { Map, GoogleApiWrapper, Polygon, Marker } from "google-maps-react";
+import {getData} from './tempData'
+//import "regenerator-runtime/runtime";
 
-/*const kazkas = {
-    height:'10vw',
-  position:'relative',
-  background: 'black',
+const mapStyles = {
+  width: "100vw",
+  height: "100vw",
+  position: "relative",
+  margin: 0,
+  padding: 0
 };
-*/const mapStyles = {
-    width: '100vw',
-    height: '100vw',
-    position: 'relative',
-    margin: 0,
-    padding: 0,
-  };
 
-class MapChart extends React.Component{
-    render() {
-        return (
-          <div>
-            <Map
-              google={this.props.google}
-              zoom={8}
-              style={mapStyles}
-              initialCenter={{ lat: 47.444, lng: -122.176}}
+const getAreaInfo = () => {
+  return JSON.parse(getData()).features;
+};
+
+const geoJsonToCoords = (geoJson) => {
+  if (geoJson.geometry.type == "Point"){
+    return {
+      lat: geoJson.geometry.coordinates[0],
+      lng: geoJson.geometry.coordinates[1],
+    }
+  } else {
+    return geoJson.geometry.coordinates[0].map(coords => ({
+      lat: coords[0],
+      lng: coords[1],
+    }))
+  }
+}
+
+class MapChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      areas: getAreaInfo(),
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Map
+          google={this.props.google}
+          zoom={4}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176 }}
+        >
+          {this.state.areas.map((obj, index) => {
+            console.log(obj);
+            console.log(geoJsonToCoords(obj));
+            return obj.geometry.type == "Point" ? <Marker position={geoJsonToCoords(obj)} name={index} key={index}/> : <Polygon
+              paths={geoJsonToCoords(obj)}
+              strokeColor="#0000FF"
+              strokeOpacity={0.8}
+              strokeWeight={2}
+              fillColor="#0000FF"
+              fillOpacity={0.35}
+              key={index}
             />
-            </div>
-        );
-      }
+            }
+          )
+          }
+        </Map>
+      </div>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
-    apiKey: 'AIzaSyAyNJ3YFC0jTvxEBj5uSU1mc7kjNIcZqck'
-  })(MapChart);
+  apiKey: "AIzaSyAyNJ3YFC0jTvxEBj5uSU1mc7kjNIcZqck"
+})(MapChart);
